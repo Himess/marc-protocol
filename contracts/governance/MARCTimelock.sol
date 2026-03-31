ac89e311b3fc0116bd6649990f9d117d4bf01b46
Himess <semihcvlk53@gmail.com>
2026-03-31 14:52:01 +0300

    Fix critical contract issues: IConfidentialACP interface, hooks, deprecate old ACP, custom errors

--- contracts/governance/MARCTimelock.sol
+++ contracts/governance/MARCTimelock.sol
@@ -10,6 +10,10 @@ import "@openzeppelin/contracts/governance/TimelockController.sol";
 /// @dev    The Gnosis Safe multisig is set as both proposer and executor.
 ///         The deployer EOA should NOT be an admin after setup.
 contract MARCTimelock is TimelockController {
+    error InvalidDelay();
+    error NoProposers();
+    error NoExecutors();
+
     /// @param minDelay Minimum delay in seconds (recommend 48 hours = 172800)
     /// @param proposers Addresses that can propose operations (Gnosis Safe)
     /// @param executors Addresses that can execute operations (Gnosis Safe)
@@ -20,8 +24,8 @@ contract MARCTimelock is TimelockController {
         address[] memory executors,
         address admin
     ) TimelockController(minDelay, proposers, executors, admin) {
-        require(minDelay >= 1 hours && minDelay <= 365 days, "MARCTimelock: invalid delay");
-        require(proposers.length > 0, "MARCTimelock: no proposers");
-        require(executors.length > 0, "MARCTimelock: no executors");
+        if (minDelay < 1 hours || minDelay > 365 days) revert InvalidDelay();
+        if (proposers.length == 0) revert NoProposers();
+        if (executors.length == 0) revert NoExecutors();
     }
 }
